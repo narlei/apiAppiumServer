1cdc3f4741f48f89f97c89e3e758dfaff52bb1ed
Narlei Américo Moreira <narlei.guitar@gmail.com>
2019-01-30 13:08:01 -0200

    Add shell 2

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -1,17 +1,21 @@
 import Vapor
 import Foundation
 
+
 /// Controls basic CRUD operations on `Todo`s.
 final class TodoController {
     /// Returns a list of all `Todo`s.
     
+    
     func show(_ req: Request) throws -> String {
-        shell("code", " /docs.txt")
-        return "LALA1"
+//        shell("code", "/Users/narlei/sources/ios/app-ios-2.0")
+        let a = shellReturn(launchPath: "/usr/bin/env", arguments: ["fastlane", "teste"])
+//        shell("cd", "/Users/narlei/sources/ios/app-ios-2.0", "&", "code", ".")
+        return a ?? "Nada"
     }
     
     func index(_ req: Request) throws -> Future<[Todo]> {
-        shell("code", " /docs.txt")
+//        shell("code", " /docs.txt")
         return Todo.query(on: req).all()
     }
 
@@ -27,6 +31,22 @@ final class TodoController {
         return try req.parameters.next(Todo.self).flatMap { todo in
             return todo.delete(on: req)
         }.transform(to: .ok)
+    }
+    
+    func shellReturn(launchPath: String, arguments: [String]) -> String?
+    {
+        let task = Process()
+        task.launchPath = launchPath
+        task.arguments = arguments
+        
+        let pipe = Pipe()
+        task.standardOutput = pipe
+        task.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: String.Encoding.utf8)
+        
+        return output
     }
     
     @discardableResult
